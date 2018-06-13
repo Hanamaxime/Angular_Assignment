619658db708a6e86c1e58010aabc3509f28fa69c
Terumi Kusaka <t.kusaka3@gmail.com>
2018-06-13 16:59:18 -0400

    merge branches color and Hana

--- src/app/order/order.model.ts
+++ src/app/order/order.model.ts
@@ -2,10 +2,10 @@ export class Order {
 
 constructor (
       // public id: number;
-      public sizeBrush: string;
-      public numBrush: number;
-      public typeBrush: string[];
-      public subNews: boolean;
+      public sizeBrush: string,
+      public numBrush: number,
+      public typeBrush: string[],
+      public subNews: boolean
     ){}
 
 }
