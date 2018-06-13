9e33212d4995029481609c4d7d0ab98f0d9e2f16
Hana <hanamaxime@gmail.com>
2018-06-13 11:20:39 -0400

    html and cssfinished

--- src/app/order.service.ts
+++ src/app/order.service.ts
@@ -5,9 +5,11 @@ import { Observable, of } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class OrderService {
 
-    order:order[] = [
+    order:orders[] = [
       {id: 1, typeBrush1: 'roller', typeBrush2: 'flat'},
       {id: 2, typeBrush1: 'flat', typeBrush2: 'roller'},
       ];
--- src/app/order/order.model.ts
+++ src/app/order/order.model.ts
@@ -3,5 +3,4 @@ export class Order {
       id: number;
       typeBrush1:string;
       typeBrush2:string;
-
 }
